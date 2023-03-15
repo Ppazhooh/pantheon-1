6147690b6907b9c084560aa941290459bc978abe
Parsa Pazhooheshy <parsapzh@node-0.reminis.cc4ngn.emulab.net>
2023-03-15 14:57:24 -0600

    chage running script

--- run_local.sh
+++ run_local.sh
@@ -1,10 +1,9 @@
-result_directory="/d1"
-#rm tmp/*
+result_directory="logs"
 
-for all_log_name in /proj/CC4NGN/5G/clb_traces/*
+for all_log_name in ~/Reminis/5G_traces/*
 do
   log_name=$(basename "$all_log_name")
-  src/experiments/test.py local --schemes "reminis" -f 0 -t 5 --start-run-id 1 --buffer-size 3300 --mm-delay 10 --downlink-trace /proj/CC4NGN/5G/traces/480mb --uplink-trace /proj/CC4NGN/5G/clb_traces/$log_name --data-dir $result_directory
+  src/experiments/test.py local --schemes "reminis" -f 0 -t 60 --start-run-id 1 --buffer-size 3300 --mm-delay 10 --downlink-trace ~/Reminis/wired_traces/480mb --uplink-trace ~/Reminis/5G_traces/$log_name --data-dir $result_directory
   rm $result_directory/*acklink*
   rm $result_directory/*stats*
   rm $result_directory/*init*
@@ -14,7 +13,4 @@ do
   do
     mv "$i" "$i"_"$log_name"
   done
- # /proj/CC4NGN/5G/anal.sh
- # mv $result_directory/*mm* $result_directory/logs2
-
 done
